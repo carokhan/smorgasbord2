3695c635d7eaad48b427fb603d90305c54bfa544
Apoorva Saurav <hcps-saurava@protonmail.com>
2023-04-01 08:57:22 -0400

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM python:3.11.0
-COPY . /app
-WORKDIR /app
+WORKDIR /
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
-CMD ["main.py"]
+CMD ["run.py"]
 
 EXPOSE 80
--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from web import app
+import sys
+
+if sys.argv[1] != "80":
+    port = int(sys.argv[1])
+else:
+    port = 80
+
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=80)
+    serve(app, host="0.0.0.0", port=port)
