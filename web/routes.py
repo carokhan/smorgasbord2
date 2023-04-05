73c0d60901a8c69457efecf4794db26f89713f5f
carokhan <apoorva.saur7@gmail.com>
2023-04-05 05:28:01 -0400

    pretty

--- web/routes.py
+++ web/routes.py
@@ -5,19 +5,27 @@ from web import app
 import web.utils.data as data_tools
 import yaml
 import web.utils.graphs as graphs
+import tbapy
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import time
+import statbotics
+
+load_dotenv()
+
+with open("config.yaml", "r") as c:
+    config = yaml.load(c, Loader=yaml.Loader)
 
 with open("data/teams.yaml", "r") as f:
     teamnames = yaml.load(f, Loader=yaml.Loader)
 
-
-@app.route("/")
-def main_page():
+@app.route("/event")
+def event_page():
     data = data_tools.load_data()
 
     if data is None:
-        context = {
-        "team_list": [],
-    }
+        context = {"team_list": [], "time": datetime.now().strftime("%H:%M:%S")}
         return render_template("204.html", context=context)
 
     teams = {}
@@ -31,6 +39,7 @@ def main_page():
     teleop_average = round(data["telePoints"].mean(), 2)
     charge_average = round(data["chargePoints"].mean(), 2)
     cycles = round(data["cycles"].mean(), 2)
+    failure = round(data["tipped"].mean(), 2)
     matches = data["matchNum"].max()
 
     graph = graphs.overall_event(data)
@@ -40,15 +49,17 @@ def main_page():
         "teleop_average": teleop_average,
         "charge_average": charge_average,
         "cycles": cycles,
+        "failure": failure,
         "matches": matches,
         "team_list": sorted(list(teams.values())),
         "graph": graph,
+        "time": datetime.now().strftime("%H:%M:%S"),
     }
-    return render_template("main.html", context=context)
+    return render_template("event.html", context=context)
 
 
 @app.route("/team", methods=["POST"])
-def plot_team():
+def team_page():
     data = data_tools.load_data()
 
     teams = {}
@@ -73,6 +84,7 @@ def plot_team():
     cycle_avg = round(team_data["cycles"].mean(), 2)
 
     defense = round(team_data["defenseScore"].mean(), 2)
+    failure = round(team_data["tipped"].mean(), 2)
 
     graph = graphs.by_team(team_data)
 
@@ -84,8 +96,189 @@ def plot_team():
         "charge_avg": charge_avg,
         "cycle_avg": cycle_avg,
         "defense": defense,
+        "failure": failure,
         "graph": graph,
         "team_list": sorted(list(teams.values())),
+        "time": datetime.now().strftime("%H:%M:%S"),
     }
 
     return render_template("team.html", context=context)
+
+
+@app.route("/")
+def pit_page():
+    data = data_tools.load_data()
+    teams = {}
+    for num in list(data["teamNumber"]):
+        try:
+            teams[int(num)] = teamnames[int(num)]
+        except KeyError:
+            data = data[data.teamNumber != num]
+
+    tba = tbapy.TBA(os.environ["TBA_API_KEY"])
+
+    matches = tba.event_matches(config["EVENT_CODE"])
+    matches = sorted(matches, key=lambda match: match["predicted_time"])
+
+    current = "END"
+    for match in matches:
+        if match["actual_time"] is None:
+            if match["comp_level"] != "qm":
+                current = match["comp_level"] + str(match["match_number"])
+            elif match["comp_level"] == "qm":
+                current = str(match["match_number"])
+            break
+
+    team_matches = tba.team_matches(
+        team=int(config["TEAM"]), event=config["EVENT_CODE"], year=int(config["YEAR"])
+    )
+    team_matches = sorted(team_matches, key=lambda match: match["predicted_time"])
+
+    next_team = "END"
+    next_color = ""
+    next_style = ""
+    next_time = 0
+    wins = 0
+    losses = 0
+    ties = 0
+
+    for match in team_matches:
+        if match["actual_time"] is None:
+            if match["comp_level"] != "qm":
+                next_team = match["comp_level"] + str(match["match_number"])
+            elif match["comp_level"] == "qm":
+                next_team = str(match["match_number"])
+
+            if "frc" + str(config["TEAM"]) in match["alliances"]["blue"]["team_keys"]:
+                next_color = "Blue in"
+                next_style = "; color:dodgerblue"
+            elif "frc" + str(config["TEAM"]) in match["alliances"]["red"]["team_keys"]:
+                next_color = "Red in"
+                next_style = "; color:crimson"
+            else:
+                next_color = ""
+                next_style = ""
+
+            next_time = round(
+                (match["predicted_time"] - time.mktime(datetime.now().timetuple())) / 60
+            )
+
+        elif match["actual_time"] is not None:
+            win = match["winning_alliance"]
+            if win == "":
+                ties += 1
+            elif "frc" + str(config["TEAM"]) in match["alliances"][win]["team_keys"]:
+                wins += 1
+            else:
+                losses += 1
+
+        record = str(wins) + "-" + str(losses) + "-" + str(ties)
+
+    upcoming = [match for match in team_matches if match["actual_time"] is None]
+
+    nums = [match["key"].split("_")[-1] for match in upcoming]
+    red1 = [match["alliances"]["red"]["team_keys"][0][3:] for match in upcoming]
+    red2 = [match["alliances"]["red"]["team_keys"][1][3:] for match in upcoming]
+    red3 = [match["alliances"]["red"]["team_keys"][2][3:] for match in upcoming]
+    blue1 = [match["alliances"]["blue"]["team_keys"][0][3:] for match in upcoming]
+    blue2 = [match["alliances"]["blue"]["team_keys"][1][3:] for match in upcoming]
+    blue3 = [match["alliances"]["blue"]["team_keys"][2][3:] for match in upcoming]
+    etas = [
+        str(
+            round(
+                (match["predicted_time"] - time.mktime(datetime.now().timetuple())) / 60
+            )
+        )
+        + " min"
+        for match in upcoming
+    ]
+
+    schedule = {
+        "#": nums,
+        "Red 1": red1,
+        "Red 2": red2,
+        "Red 3": red3,
+        "Blue 1": blue1,
+        "Blue 2": blue2,
+        "Blue 3": blue3,
+        "ETA": etas,
+    }
+    df = pd.DataFrame(schedule)
+
+    context = {
+        "current": current,
+        "next_team": next_team,
+        "next_color": next_color,
+        "next_style": next_style,
+        "next_time": next_time,
+        "record": record,
+        "team": str(config["TEAM"]),
+        "columns": df.columns.values,
+        "rows": list(df.values.tolist()),
+        "zip": zip,
+        "time": datetime.now().strftime("%H:%M:%S"),
+        "team_list": sorted(list(teams.values())),
+    }
+
+    return render_template("pit.html", context=context)
+
+
+@app.route("/playground", methods=["POST"])
+def alliance_page():
+    data = data_tools.load_data()
+
+    teams = {}
+    for num in list(data["teamNumber"]):
+        try:
+            teams[int(num)] = teamnames[int(num)]
+        except KeyError:
+            data = data[data.teamNumber != num]
+
+    red_alliance = [
+        request.form["red1"].split(" ")[0],
+        request.form["red2"].split(" ")[0],
+        request.form["red3"].split(" ")[0],
+    ]
+
+    blue_alliance = [
+        request.form["blue1"].split(" ")[0],
+        request.form["blue2"].split(" ")[0],
+        request.form["blue3"].split(" ")[0],
+    ]
+
+    sb = statbotics.Statbotics()
+
+    redEPA = sum([sb.get_team_year(int(team), int(config["YEAR"]), ["epa_end"])["epa_end"] for team in red_alliance])
+    blueEPA = sum([sb.get_team_year(int(team), int(config["YEAR"]), ["epa_end"])["epa_end"] for team in blue_alliance])
+
+    red_score = round(redEPA * 1.065)
+    blue_score = round(blueEPA * 1.065)
+
+    percentage = round(100 / (1 + 10 ** ((-5/8 * (redEPA - blueEPA)) / sb.get_year(int(config["YEAR"]), ["score_sd"])["score_sd"])), 2)
+
+    if percentage < 50:
+        percentage = 100 - percentage
+
+    lookup = lambda x: data[data.teamNumber == x]
+    red_dfs = [lookup(int(team)) for team in red_alliance]
+    blue_dfs = [lookup(int(team)) for team in blue_alliance]
+
+    graph = graphs.alliance_graph(red_dfs, blue_dfs)
+
+    context = {
+        "red1": red_alliance[0],
+        "red2": red_alliance[1],
+        "red3": red_alliance[2],
+        "blue1": blue_alliance[0],
+        "blue2": blue_alliance[1],
+        "blue3": blue_alliance[2],
+        "red_score": red_score,
+        "blue_score": blue_score,
+        "percentage": percentage,
+        "time": datetime.now().strftime("%H:%M:%S"),
+        "team_list": sorted(list(teams.values())),
+        "graph": graph
+    }
+
+
+    return render_template("alliances.html", context=context)
--- web/utils/graphs.py
+++ web/utils/graphs.py
@@ -14,8 +14,6 @@ def overall_event(df):
     avgs = df.groupby(["teamNumber"]).mean().round(2)
     avgs["teamName"] = avgs.apply(lambda row: teamnames[int(row.teamNumber2)], axis=1)
 
-    print(avgs.head())
-
     fig = px.scatter(
         avgs,
         x="telePoints",
@@ -78,8 +76,8 @@ def overall_event(df):
 def by_team(df):
     df = df.sort_values("matchNum")
 
-    fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.02)
-    
+    fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.125)
+
     fig.add_trace(
         go.Scatter(
             x=list(df["matchNum"]),
@@ -122,7 +120,7 @@ def by_team(df):
             "yanchor": "top",
         },
         width=1500,
-        height=425,
+        height=550,
         paper_bgcolor="rgba(255,255,255,0)",
         plot_bgcolor="rgba(255,255,255,0)",
         font=dict(color="#777"),
@@ -148,6 +146,141 @@ def by_team(df):
     fig.update_yaxes(title_text="auto cargo", row=1, col=1)
     fig.update_yaxes(title_text="teleop cargo", row=2, col=1)
     fig.update_yaxes(title_text="endgame level", row=3, col=1)
+
+    plot_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return plot_json
+
+
+def alliance_graph(red_dfs, blue_dfs):
+    layout = go.Layout(polar=dict(radialaxis=dict(visible=True, range=[0, 1])))
+
+    fig = make_subplots(
+        rows=2,
+        cols=4,
+        vertical_spacing=0.25,
+        specs=[[{"type": "polar"}] * 4] * 2,
+        horizontal_spacing=0.05,
+    )
+    fields = [
+        "autoPointsNormalized",
+        "telePointsNormalized",
+        "chargePointsNormalized",
+        "defenseScoreNormalized",
+        "cyclesNormalized",
+    ]
+
+    labels = ["auto", "teleop", "charge", "defense", "cycles"]
+    reds = ["crimson", "orangered", "tomato"]
+    blues = ["dodgerblue", "royalblue", "steelblue"]
+
+    col = 2
+    color = 0
+
+    fig.add_trace(
+        go.Scatterpolar(
+            r=[round(red_dfs[0][field].mean()) + round(red_dfs[1][field].mean()) + round(red_dfs[2][field].mean()) for field in fields],
+            theta=labels,
+            fill="toself",
+            name="Red",
+            text="Red",
+            line=dict(color="crimson"),
+            marker=dict(color="crimson")
+        ),
+        row=1,
+        col=1
+    )
+
+    for red_df in red_dfs:
+        fig.add_trace(
+            go.Scatterpolar(
+                r=[round(red_df[field].mean(), 2) for field in fields],
+                theta=labels,
+                fill="toself",
+                name=str(round(red_df["teamNumber"].mean())),
+                text=str(round(red_df["teamNumber"].mean())),
+                line=dict(color=reds[color]),
+                marker=dict(color=reds[color])
+            ),
+            row=1,
+            col=col,
+        )
+
+        # fig.add_trace(
+        #     go.Scatterpolar(
+        #         r=[round(red_df[field].mean(), 2) for field in fields],
+        #         theta=labels,
+        #         fill="toself",
+        #         name=str(round(red_df["teamNumber"].mean())),
+        #         line=dict(color=reds[color]),
+        #         marker=dict(color=reds[color])
+        #     ),
+        #     row=1,
+        #     col=1,
+        # )
+
+        col += 1
+
+    col = 2
+    color = 0
+
+    fig.add_trace(
+        go.Scatterpolar(
+            r=[round(blue_dfs[0][field].mean()) + round(blue_dfs[1][field].mean()) + round(blue_dfs[2][field].mean()) for field in fields],
+            theta=labels,
+            fill="toself",
+            name="Blue",
+            text="Blue",
+            line=dict(color="dodgerblue"),
+            marker=dict(color="dodgerblue")
+        ),
+        row=2,
+        col=1
+    )
+
+    for blue_df in blue_dfs:
+        fig.add_trace(
+            go.Scatterpolar(
+                r=[round(blue_df[field].mean(), 2) for field in fields],
+                theta=labels,
+                fill="toself",
+                name=str(round(blue_df["teamNumber"].mean())),
+                text=str(round(blue_df["teamNumber"].mean())),
+                line=dict(color=blues[color]),
+                marker=dict(color=blues[color])
+            ),
+            row=2,
+            col=col,
+        )
+
+        # fig.add_trace(
+        #     go.Scatterpolar(
+        #         r=[round(blue_df[field].mean(), 2) for field in fields],
+        #         theta=labels,
+        #         fill="toself",
+        #         name=str(round(blue_df["teamNumber"].mean())),
+        #         line=dict(color=blues[color]),
+        #         marker=dict(color=blues[color])
+        #     ),
+        #     row=2,
+        #     col=1,
+        # )
+
+        col += 1
+
+    polar_layouts = {"polar" + str(i): dict(radialaxis=dict(visible=True, range=[0, 1])) if i%4 != 1 else dict(radialaxis=dict(visible=True, range=[0, 3])) for i in range(1, 9)}
+
+    titles = ["Red", str(round(red_dfs[0]["teamNumber"].mean())), str(round(red_dfs[1]["teamNumber"].mean())), str(round(red_dfs[2]["teamNumber"].mean())), "Blue", str(round(blue_dfs[0]["teamNumber"].mean())), str(round(blue_dfs[1]["teamNumber"].mean())), str(round(blue_dfs[2]["teamNumber"].mean()))]
+    title_layouts = {"title" + str(i+1): dict(text=titles[i]) for i in range(len(titles))}
+
+    fig.update_layout(
+        width=1500,
+        height=650,
+        paper_bgcolor="rgba(255,255,255,0)",
+        plot_bgcolor="rgba(255,255,255,0)",
+        font=dict(color="#777"),
+        showlegend=False,
+        **polar_layouts
+                )
 
     plot_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return plot_json
